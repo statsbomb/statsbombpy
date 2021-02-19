51f80004c1384656c281a74e8493b3bdaa07cebd
Fran <frangoitia@gmail.com>
2021-02-19 15:07:25 -0300

    display particular events attributes as columns of main dataframe

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 
 setup(
     name="statsbombpy",
-    version="0.1",
+    version="1.0",
     description="easily stream StatsBomb data into Python",
+    long_description="easily stream StatsBomb data into Python",
     url="https://github.com/statsbomb/statsbombpy",
     author="StatsBomb",
     author_email="support@statsbombservices.com",
