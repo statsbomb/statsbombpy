cca1f660dc07876fa6a8623f2107a0052a244fb5
Fran <frangoitia@gmail.com>
2021-02-19 15:30:05 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from setuptools import setup
 
 setup(
     name="statsbombpy",
-    version="0.1",
+    version="0.2",
     description="easily stream StatsBomb data into Python",
+    long_description="easily stream StatsBomb data into Python",
     url="https://github.com/statsbomb/statsbombpy",
+    download_url="https://github.com/statsbomb/statsbombpy/archive/v0.2.tar.gz",
     author="StatsBomb",
     author_email="support@statsbombservices.com",
     packages=["statsbombpy"],
-    install_requires=["cashier", "inflect", "nose2", "pandas", "requests", "requests-cache"],
+    install_requires=[
+        "cashier",
+        "inflect",
+        "nose2",
+        "pandas",
+        "requests",
+        "requests-cache",
+    ],
 )
