69ef0034b5f483232be03c2d3b0ff3b5c1cde527
Fran <frangoitia@gmail.com>
2021-02-23 10:47:35 -0300

    update description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
+
 from setuptools import setup
 
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")) as f:
+    README = f.read()
 
 setup(
     name="statsbombpy",
-    version="0.2",
+    version="0.3",
     description="easily stream StatsBomb data into Python",
-    long_description="easily stream StatsBomb data into Python",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/statsbomb/statsbombpy",
     download_url="https://github.com/statsbomb/statsbombpy/archive/v0.2.tar.gz",
     author="StatsBomb",
