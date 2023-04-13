9eb33bd5e73debfdc16068a02f5658a6e549d4f8
Scott <scott.johnson@statsbomb.com>
2023-04-13 18:29:24 +0100

    Update events API version number

--- statsbombpy/config.py
+++ statsbombpy/config.py
@@ -30,7 +30,7 @@ VERSIONS = {
     "competitions": "v4",
     "matches": "v5",
     "lineups": "v4",
-    "events": "v7",
+    "events": "v8",
     "360-frames": "v2",
     "player-match-stats": "v4",
     "player-season-stats": "v4",
